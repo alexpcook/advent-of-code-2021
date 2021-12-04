3ce4480de0ed7a7e1fa442fc7501b8edc45abfd7
Alex Cook <apcook7104@gmail.com>
2021-12-03 17:47:34 -0800

    refactor day 3 to make part 2 share code with part 1

--- src/days/day_3.rs
+++ src/days/day_3.rs
@@ -3,19 +3,11 @@ use diagnostic::BinaryDiagnostic;
 pub fn main() {
     println!("### day 3 ###");
 
-    // Part 1
-    println!(
-        "part 1: {}",
-        BinaryDiagnostic::power_consumption("./day_3.txt")
-            .expect("could not calculate power consumption")
-    );
+    let binary_diagnostic =
+        BinaryDiagnostic::from_file("./day_3.txt").expect("could not read input file");
 
-    // Part 2
-    println!(
-        "part 2: {}",
-        BinaryDiagnostic::life_support_rating("./day_3.txt")
-            .expect("could not calculate life support rating")
-    );
+    // Part 1
+    println!("part 1: {}", binary_diagnostic.power_consumption());
 }
 
 mod diagnostic {
@@ -23,58 +15,77 @@ mod diagnostic {
     use std::io::{self, BufRead};
     use std::path::Path;
 
-    /// The number of binary digits in one line of input.
+    /// The number of binary digits in one line of diagnostic input data.
     static BINARY_LINE_LENGTH: usize = 12;
 
     /// The binary diagnostic info for the submarine.
-    pub struct BinaryDiagnostic;
+    pub struct BinaryDiagnostic {
+        data: Vec<String>,
+    }
 
     impl BinaryDiagnostic {
-        /// Calculate the power consumption of the submarine from an input file at `path`.
-        pub fn power_consumption(path: &str) -> io::Result<u32> {
-            let (gamma_rate, epsilon_rate) = io::BufReader::new(File::open(Path::new(path))?)
+        /// Initializes the submarine's binary diagnostic readings from an input file at `path`.
+        pub fn from_file(path: &str) -> io::Result<BinaryDiagnostic> {
+            let data = io::BufReader::new(File::open(Path::new(path))?)
                 .lines()
                 .map(|l| l.unwrap_or_default())
-                // Flatten the input file into a vector of characters
-                .flat_map(|s| s.chars().collect::<Vec<_>>())
-                .enumerate()
-                // Fold the vector of characters into another vector with BINARY_LINE_LENGTH
-                // number of entries (one for each column of data we need to read)
-                // Increment each column's entry by 1 if '1', else decrement by 1 if '0'
-                .fold(vec![0; BINARY_LINE_LENGTH], |mut res, (i, c)| {
-                    res[i % BINARY_LINE_LENGTH] += if c == '1' { 1 } else { -1 };
-                    res
-                })
-                .into_iter()
-                // Fold the vector into a tuple for the gamma and epsilon rates
-                .fold(
-                    (
-                        String::with_capacity(BINARY_LINE_LENGTH),
-                        String::with_capacity(BINARY_LINE_LENGTH),
-                    ),
-                    |(mut gamma, mut epsilon), i| {
-                        let (g, e) = match i {
-                            // 1 is most common entry in the column
-                            1.. => ('1', '0'),
-                            // 0 is the most common entry in the column
-                            _ => ('0', '1'),
-                        };
-                        gamma.push(g);
-                        epsilon.push(e);
-                        (gamma, epsilon)
-                    },
-                );
+                .collect::<Vec<_>>();
+            Ok(BinaryDiagnostic { data })
+        }
 
-            // Parse the gamma and epsilon strings into binary numbers
+        /// Calculates the power consumption of the submarine.
+        pub fn power_consumption(&self) -> u32 {
+            let mut gamma_rate = String::with_capacity(BINARY_LINE_LENGTH);
+            let mut epsilon_rate = String::with_capacity(BINARY_LINE_LENGTH);
+
+            for i in 0..BINARY_LINE_LENGTH {
+                let (g, e) = match Self::most_common_bit(&self.data, i) {
+                    Some(x) if x == 1 => ('1', '0'),
+                    Some(x) if x == 0 => ('0', '1'),
+                    _ => continue,
+                };
+                gamma_rate.push(g);
+                epsilon_rate.push(e);
+            }
+
             let gamma_rate = u32::from_str_radix(&gamma_rate, 2).unwrap_or(0);
             let epsilon_rate = u32::from_str_radix(&epsilon_rate, 2).unwrap_or(0);
 
-            Ok(gamma_rate * epsilon_rate)
+            gamma_rate * epsilon_rate
         }
 
-        /// Calculate the life support rating of the submarine from an input file at `path`.
-        pub fn life_support_rating(path: &str) -> io::Result<u32> {
-            Ok(0)
+        /// Calculates the most common bit from `data` at `position`.
+        /// Returns `Some(1)` if `1` is most common, `Some(0)` if `0` is most common,
+        /// and `None` if `1` and `0` are equally common.
+        /// # Panics
+        /// Panics if `position >= BINARY_LINE_LENGTH`.
+        fn most_common_bit(data: &[String], position: usize) -> Option<u8> {
+            if position >= BINARY_LINE_LENGTH {
+                panic!(
+                    "cannot equal or exceed BINARY_LINE_LENGTH {}",
+                    BINARY_LINE_LENGTH
+                );
+            }
+
+            let result = data
+                .iter()
+                .flat_map(|s| s.chars().collect::<Vec<_>>())
+                .skip(position)
+                .step_by(BINARY_LINE_LENGTH)
+                .fold(0, |mut acc, c| {
+                    if c == '1' {
+                        acc += 1;
+                    } else {
+                        acc -= 1;
+                    }
+                    acc
+                });
+
+            match result {
+                0 => None,
+                1.. => Some(1),
+                _ => Some(0),
+            }
         }
     }
 }
