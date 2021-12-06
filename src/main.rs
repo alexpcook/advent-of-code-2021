adf9b99c24c8d12dca4a4224bdfbe10316663916
Alex Cook <apcook7104@gmail.com>
2021-12-05 16:23:07 -0800

    add day 6 skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use advent_of_code_2021::days;
 
 fn main() {
-    days::day_1::main();
-    days::day_2::main();
-    days::day_3::main();
-    days::day_4::main();
+    // days::day_1::main();
+    // days::day_2::main();
+    // days::day_3::main();
+    // days::day_4::main();
     days::day_5::main();
+    days::day_6::main();
 }
