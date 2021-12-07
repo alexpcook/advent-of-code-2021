baee3aeb4c9e40ee66b1f7d3ae287f35b422acd0
Alex Cook <apcook7104@gmail.com>
2021-12-06 20:46:30 -0800

    add constants for day 6, parts 1 and 2

--- src/days/day_6.rs
+++ src/days/day_6.rs
@@ -1,7 +1,10 @@
 use std::fs;
 
-/// Number of days to simulate.
-const DAYS: u32 = 80;
+/// Number of days to simulate for part 1.
+const DAYS_PART_1: u32 = 80;
+
+/// Number of days to simulate for part 2.
+const DAYS_PART_2: u32 = 256;
 
 pub fn main() {
     let contents = fs::read_to_string("./day_6.txt").expect("could not read input file");
@@ -15,7 +18,8 @@ pub fn main() {
         .flatten()
         .collect();
 
-    for day in 1..=DAYS {
+    // Part 1
+    for day in 1..=DAYS_PART_1 {
         let mut new_fish = vec![];
 
         for fish in lanternfish.iter_mut() {
