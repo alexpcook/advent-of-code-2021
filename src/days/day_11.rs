cb895a39a2f6f2c7d04b6e62f7819040a3b74158
Alex Cook <apcook7104@gmail.com>
2021-12-11 13:41:40 -0800

    add more methods for octopus and pod

--- src/days/day_11.rs
+++ src/days/day_11.rs
@@ -3,11 +3,15 @@ use std::path::Path;
 
 pub fn main() {
     let input_path = Path::new("./day_11.txt");
-    let pod = Pod::from_file(input_path).expect("could not read input file");
+    let mut pod = Pod::from_file(input_path).expect("could not read input file");
+
+    // Part 1
+    println!("part 1: flashes after 100 steps = {}", pod.simulate(100));
 }
 
 mod octopus {
     use std::collections::HashMap;
+    use std::fmt;
     use std::fs;
     use std::io;
     use std::path::Path;
@@ -39,11 +43,11 @@ mod octopus {
 
     /// Models a pod of octopus.
     #[derive(Debug)]
-    pub struct Pod(HashMap<(usize, usize), u32>);
+    pub struct Pod(HashMap<(usize, usize), Octopus>);
 
     impl Pod {
-        /// Constructs a pod of octopus from input `path`.
-        pub fn from_file(path: &Path) -> io::Result<HashMap<(usize, usize), Octopus>> {
+        /// Constructs a `Pod` of octopus from input `path`.
+        pub fn from_file(path: &Path) -> io::Result<Pod> {
             let contents = fs::read_to_string(path)?;
             let mut octopus = HashMap::with_capacity(POD_SIZE * POD_SIZE);
 
@@ -61,7 +65,50 @@ mod octopus {
                 }
             }
 
-            Ok(octopus)
+            Ok(Pod(octopus))
+        }
+
+        /// Simulates a `Pod` for a given number of `steps`.
+        pub fn simulate(&mut self, steps: u32) -> u32 {
+            let mut flashed = 0;
+            for _ in 0..steps {
+                flashed += self.step();
+                self.reset();
+            }
+            flashed
+        }
+
+        /// Increments the state of all `Octopus` in the `Pod`.
+        fn step(&mut self) -> u32 {
+            let mut flashed = 0;
+            for octopus in self.0.values_mut() {
+                let new_state = octopus.step();
+                if new_state == 10 {
+                    flashed += 1;
+                }
+            }
+            flashed
+        }
+
+        /// Resets all `Octopus` in the `Pod` that flashed.
+        fn reset(&mut self) {
+            for octopus in self.0.values_mut() {
+                if octopus.0 > 9 {
+                    octopus.reset();
+                }
+            }
+        }
+    }
+
+    impl fmt::Display for Pod {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            for i in 0..POD_SIZE {
+                for j in 0..POD_SIZE {
+                    write!(f, "{}", self.0.get(&(i, j)).unwrap_or(&Octopus::new(0)).0)?;
+                }
+                writeln!(f)?;
+            }
+            Ok(())
         }
     }
 }
